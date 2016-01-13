7c255fc9b335ef3da6e5153b1ceeb952e7fc7555
Mario Godinez Medina <futureweb2000@gmail.com>
2016-01-13 14:12:16 -0600

    el avance del proyecto de bienal

---
+++ app.js
@@ -0,0 +1,21 @@
+(function(){
+	
+var app = angular.module('bienal',[]);
+
+		app.controller('PosterController',['$scope', '$http', function($scope, $http){
+			$scope.posts = [];
+			$http.get('/data.json')
+				.success(function(data){
+					$scope.posts = data;
+				})
+
+		setTimeout(function(){
+			console.log($scope.posts.length)
+		}, 3000)		
+
+
+		}])
+
+
+})();
+
